4f01e8cd539d5e70ba9f3f73422ff29ac2b70507
arome88 <anaromero.88@icloud.com>
2023-05-07 19:05:24 -0400

    marco-tracker

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'url' : 'mongodb+srv://agrome88:cardiotracker1@cluster0.3p1cl9m.mongodb.net/test', 
     'dbName': 'demo'
 };
